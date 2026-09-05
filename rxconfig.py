7436dba2e506f09ee1a51204d3aee6df38691400
Lefika-Molebatsi <lfkmolebatsi@gmail.com>
2026-09-05 14:08:07 +0200

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,10 @@
+import os
 import reflex as rx
 import reflex_xy
 
 config = rx.Config(
     app_name="app",
+    port=int(os.getenv("PORT", 3000)),
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
